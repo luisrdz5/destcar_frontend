653e77dc6fb983aaecbd7842a05e7d193a7e7cd4
Luis Antonio Rodriguez Garcia <luis_rdz5@hotmail.com>
2020-06-28 11:12:08 -0500

    fixed cookies bugs

--- src/ssr-server/config/index.js
+++ src/ssr-server/config/index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
 const config = {
-  dev: process.env.NODE_ENV !== 'production',
+  //dev: process.env.NODE_ENV !== 'production',
+  dev: true,
   port: process.env.PORT || 8000,
   apiUrl: process.env.API_URL,
   apiKeyToken: process.env.API_KEY_TOKEN,
--- src/ssr-server/server.js
+++ src/ssr-server/server.js
@@ -81,7 +81,7 @@ app.post('/auth/sign-in', async (req, res, next) => {
         const { token, ...user } = data;
         res.cookie('token', token, {
           httpOnly: !config.dev,
-          secure: !config.dev
+          secure: !config.dev,
         });
         res.status(200).json(user.user);
       });
